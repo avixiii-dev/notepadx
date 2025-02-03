8b900d5e316b4d38d81decfd5ef6dfcd7c0295c8
Tuan <avixiii@proton.me>
2025-02-03 16:36:27 +0700

    fix: add missing import for LogicalKeyboardKey

--- lib/screens/editor_screen.dart
+++ lib/screens/editor_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import '../providers/editor_provider.dart';
 import '../widgets/recent_files_menu.dart';
